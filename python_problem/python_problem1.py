d7c8eaff8e38c10c63e2e6a27d4287e1a7c7245a
Makyra Kim <makyra20412@gmail.com>
2022-07-08 10:25:39 -0500

    1-3단계

--- python_problem/python_problem1.py
+++ python_problem/python_problem1.py
@@ -1,5 +1,15 @@
 import random
 
 num = 0
-get = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : '))
-
+while True:
+    # the while True loops allows a block of code to be executed repeatedly until
+    # we get a False boolean or a break
+    try:
+        get = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : '))
+    except ValueError:
+        print('정수를 입력하세요')
+    else:
+        if get < 1 or get > 3:
+            print('1,2,3 중 하나를 입력하세요')
+        else:
+            break
