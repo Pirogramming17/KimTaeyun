35a1aeb5475ec7a9327f15ca851e26006231bdb1
Makyra Kim <makyra20412@gmail.com>
2022-07-08 10:29:10 -0500

    1-4단계

--- python_problem/python_problem1.py
+++ python_problem/python_problem1.py
@@ -5,11 +5,15 @@ while True:
     # the while True loops allows a block of code to be executed repeatedly until
     # we get a False boolean or a break
     try:
-        get = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : '))
+        numChoice = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : '))
     except ValueError:
         print('정수를 입력하세요')
     else:
-        if get < 1 or get > 3:
+        if numChoice < 1 or numChoice > 3:
             print('1,2,3 중 하나를 입력하세요')
         else:
             break
+
+for i in range(numChoice):
+    num += 1
+    print(num)
