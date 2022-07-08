6ee488e79288884a7b1525a9c523749b01a66e35
Makyra Kim <makyra20412@gmail.com>
2022-07-08 11:19:48 -0500

    1-5 단계

--- python_problem/python_problem1.py
+++ python_problem/python_problem1.py
@@ -2,38 +2,43 @@ import random
 
 num = 0
 
-while True:
-    # the while True loops allows a block of code to be executed repeatedly until
-    # we get a False boolean or a break
-    try:
-        numChoice = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : '))
-    except ValueError:
-        print('정수를 입력하세요')
-    else:
-        if numChoice < 1 or numChoice > 3:
-            print('1,2,3 중 하나를 입력하세요')
+while num < 31:
+    while True:
+        # the while True loops allows a block of code to be executed repeatedly until
+        # we get a False boolean or a break
+        try:
+            numChoice = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : '))
+        except ValueError:
+            print('정수를 입력하세요')
         else:
+            if numChoice < 1 or numChoice > 3:
+                print('1,2,3 중 하나를 입력하세요')
+            else:
+                break
+
+    for i in range(numChoice):
+        num += 1
+        print('playerA : ', num, sep='')
+        if num == 31:
+            print('playerB win!')
             break
 
-for i in range(numChoice):
-    num += 1
-    print('playerA : ', num, sep='')
-
-while True:
-    # the while True loops allows a block of code to be executed repeatedly until
-    # we get a False boolean or a break
-    try:
-        numChoice = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : '))
-    except ValueError:
-        print('정수를 입력하세요')
-    else:
-        if numChoice < 1 or numChoice > 3:
-            print('1,2,3 중 하나를 입력하세요')
+    while True:
+        # the while True loops allows a block of code to be executed repeatedly until
+        # we get a False boolean or a break
+        try:
+            numChoice = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : '))
+        except ValueError:
+            print('정수를 입력하세요')
         else:
+            if numChoice < 1 or numChoice > 3:
+                print('1,2,3 중 하나를 입력하세요')
+            else:
+                break
+
+    for i in range(numChoice):
+        num += 1
+        print('playerB : ', num, sep='')
+        if num == 31:
+            print('playerB win!')
             break
-
-for i in range(numChoice):
-    num += 1
-    print('playerB : ', num, sep='')
-
-
