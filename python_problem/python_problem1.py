2ca83e4757e43ec284828f0dde1a8561ba39784e
Makyra <makyrakim@utexas.edu>
2022-07-08 10:41:17 -0500

    1-5 단계

--- python_problem/python_problem1.py
+++ python_problem/python_problem1.py
@@ -1,6 +1,7 @@
 import random
 
 num = 0
+
 while True:
     # the while True loops allows a block of code to be executed repeatedly until
     # we get a False boolean or a break
@@ -16,4 +17,23 @@ while True:
 
 for i in range(numChoice):
     num += 1
-    print(num)
+    print('playerA : ', num, sep='')
+
+while True:
+    # the while True loops allows a block of code to be executed repeatedly until
+    # we get a False boolean or a break
+    try:
+        numChoice = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : '))
+    except ValueError:
+        print('정수를 입력하세요')
+    else:
+        if numChoice < 1 or numChoice > 3:
+            print('1,2,3 중 하나를 입력하세요')
+        else:
+            break
+
+for i in range(numChoice):
+    num += 1
+    print('playerB : ', num, sep='')
+
+
